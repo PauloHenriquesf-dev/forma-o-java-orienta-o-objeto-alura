9f61078036f9ad609e179e48fa475895c22eda94
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-01-14 00:45:45 -0300

    criando aula02: ensinando a declarar, atribuir e imprimir variavel

---
+++ mod1-JREeJDK/aula2-sintaxe-variaveis-e-fluxo/src/Testa01Variaveis.java
@@ -0,0 +1,16 @@
+
+public class Testa01Variaveis {
+	
+	public static void main(String[] args) {
+		
+		//declaração de uma variavel do tipo int:
+		int idade;
+		
+		//atribuição (coloca 29 dentro de idade):
+		idade = 29;
+		
+		//imprimindo o valor da variavel idade:
+		System.out.println("idade: " + idade);
+		
+	}
+}
