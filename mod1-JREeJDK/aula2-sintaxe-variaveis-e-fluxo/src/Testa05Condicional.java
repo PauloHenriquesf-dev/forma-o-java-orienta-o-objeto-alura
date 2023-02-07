1ff603cd64e26e379bc073113d928a129c6812a4
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-02-07 03:58:26 -0300

    Criando classe Condicional: usando if e else

---
+++ mod1-JREeJDK/aula2-sintaxe-variaveis-e-fluxo/src/Testa05Condicional.java
@@ -0,0 +1,16 @@
+
+public class Testa05Condicional {
+	
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		
+		//verifica se idade é maior ou igual 18
+		if (idade >= 18) {
+			System.out.println("Tem mais de 18 anos");
+		} else { // se nao for, faça isso
+			System.out.println("Tem menos de 18 anos");
+		}
+		
+	}
+}
