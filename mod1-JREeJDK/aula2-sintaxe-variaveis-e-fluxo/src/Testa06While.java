18acc3d125842aeb0262009a99eb425d068497da
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-02-09 17:23:41 -0300

    Criando classe While: utilizando o laço while

---
+++ mod1-JREeJDK/aula2-sintaxe-variaveis-e-fluxo/src/Testa06While.java
@@ -0,0 +1,17 @@
+
+public class Testa06While {
+	
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		
+		//enquanto contador for <= 10 faça isso
+		while(contador <= 10) {
+			System.out.println(contador);
+			
+			//somando +1 no contador a cada laço
+			contador++;
+		}
+		
+	}
+}
