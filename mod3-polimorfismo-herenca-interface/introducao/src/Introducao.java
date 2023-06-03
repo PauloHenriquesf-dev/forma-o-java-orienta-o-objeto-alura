ac8360c7e2935ddb0847f5f9a0d5338399651c86
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-06-03 03:17:42 -0300

    Explicando o que sera abordado no módulo

---
+++ mod3-polimorfismo-herenca-interface/introducao/src/Introducao.java
@@ -0,0 +1,13 @@
+
+public class Introducao {
+	
+	public static void main(String[] args) {
+		
+		/* Veremos HERANÇA e o uso de INTERFACES
+		 * Reutilização de codigos
+		 * POLIMORFISMO
+		 * Reescrita
+		 * Reutilização atraves de composicao ou heranca*/
+		
+	}
+}
